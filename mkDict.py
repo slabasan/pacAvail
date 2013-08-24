20c91b5b47321504db73b5e3dbe70b628345fd6a
Stephanie <s_labasan@u.pacific.edu>
2013-08-23 22:48:31 -0700

    Updated mkDict.py - Dictionary key is tuple length 2 where (time from 08:00 am in 5 min increments, day of week converted to corresponding number). Able to search for a professor or class within ecpe.txt (assuming, cleanedClasses.txt will work as well). All code made into functions for modularity. Import as module (mkDict).

--- mkDict.py
+++ mkDict.py
@@ -1,97 +1,84 @@
 import csv
 
-#--- make list of unique classrooms
-#mylist=[]
-#with open('ecpe.txt','rb') as csv_file:
-#	csv_reader=csv.reader(csv_file, delimiter='\t')
-#	my_list=[row[21] for row in csv_reader]
-#csv_file.close()
-#newList=list(set(my_list))
-#print newList
-#print len(newList)
-
-#--- make variables in loop
-#d={}
-#for x in newList:
-#   d["{0}".format(x)]="Hello"
-#print d
-
 #--- convert time to 5 min increment
 def timeConvert(s):
-	startH=s[0:2]
-	startM=s[3:6]
-	endH=s[9:11]
-	endM=s[12:15]
-	x=s[6:8]
-	y=s[15:17]
-	if s == "TBA":
-		resultH=0
-		resultM=0
-	elif x == "am" and y == "pm" and endH != 12:
-		newEndH=int(float(endH))+12
-		resultH=int(float(newEndH))-int(float(startH))
-		resultM=int(float(endM))-int(float(startM))
-	elif x == "pm" and y == "pm" and endH < startH:
-		newEndH=int(float(endH))+12
-		resultH=int(float(newEndH))-int(float(startH))
-		resultM=int(float(endM))-int(float(startM))
+	if "TBA" in s:
+		return -1
 	else:
-		resultH=int(float(endH))-int(float(startH))
-		resultM=int(float(endM))-int(float(startM))
-	if resultM < 0:
-		resultM+=60
-		resultH-=1
-	result_hourtomin=resultH*60
-	totaltime=result_hourtomin+resultM
-	unitconversion=totaltime/5
-	return unitconversion
+		startH=s[0:2]
+		startM=s[3:6]
+		endH=s[9:11]
+		endM=s[12:15]
+		x=s[6:8]
+		y=s[15:17]
+		if x == "am" and y == "pm" and endH != 12:
+			newEndH=int(float(endH))+12
+			resultH=int(float(newEndH))-int(float(startH))
+			resultM=int(float(endM))-int(float(startM))
+		elif x == "pm" and y == "pm" and endH < startH:
+			newEndH=int(float(endH))+12
+			resultH=int(float(newEndH))-int(float(startH))
+			resultM=int(float(endM))-int(float(startM))
+		else:
+			resultH=int(float(endH))-int(float(startH))
+			resultM=int(float(endM))-int(float(startM))
+		if resultM < 0:
+			resultM+=60
+			resultH-=1
+		result_hourtomin=resultH*60
+		totaltime=result_hourtomin+resultM
+		unitconversion=totaltime/5
+		return unitconversion
 
 def convertDay(x):
 	days=list("MWF")
 	for i in range(len(x)):
 		if x[i] == "M":
-			x[i] = 0
-		elif x[i] == "T":
 			x[i] = 1
-		elif x[i] == "W":
+		elif x[i] == "T":
 			x[i] = 2
-		elif x[i] == "R":
+		elif x[i] == "W":
 			x[i] = 3
-		elif x[i] == "F":
+		elif x[i] == "R":
 			x[i] = 4
+		elif x[i] == "F":
+			x[i] = 5
 	return x
 
 #--- create dictionary (...or list of dictionaries?)
-# {('day(8)','time(9)'): ['course(2,3)','name(7)','prof(19)','room(21)']}
-dict={}
-with open('ecpe.txt','rb') as csv_file:
-	csv_reader=csv.reader(csv_file, delimiter='\t')
-	for row in csv_reader:
-		time=timeConvert(row[9])	
-		if row[8]=="MWF":
-			days=list(row[8])
-			newDays=convertDay(days)
-			dict[newDays[0],time,row[21],row[19][:-4],row[7]]=[row[21],row[19][:-4],row[7],row[2]+" "+row[3],row[9]]
-			dict[newDays[1],time,row[21],row[19][:-4],row[7]]=[row[21],row[19][:-4],row[7],row[2]+" "+row[3],row[9]]
-			dict[newDays[2],time,row[21],row[19][:-4],row[7]]=[row[21],row[19][:-4],row[7],row[2]+" "+row[3],row[9]]
-		#elif row[8]=="TR":
-			#days=list(row[8])
-			#newDays=convertDay(days)
-			#dict[newDays[0],time,row[21],row[19][:-4],row[7]]=[row[21],row[19][:-4],row[7],row[2]+" "+row[3],row[9]]
-			#dict[newDays[1],time,row[21],row[19][:-4],row[7]]=[row[21],row[19][:-4],row[7],row[2]+" "+row[3],row[9]]
-		#elif row[8]=="MW":
-			#days=list(row[8])
-			#newDays=convertDay(days)
-			#dict[newDays[0],time,row[21],row[19][:-4],row[7]]=[row[21],row[19][:-4],row[7],row[2]+" "+row[3],row[9]]
-			#dict[newDays[1],time,row[21],row[19][:-4],row[7]]=[row[21],row[19][:-4],row[7],row[2]+" "+row[3],row[9]]
-		#elif row[8]=="WF":
-			#days=list(row[8])
-			#newDays=convertDay(days)
-			#dict[newDays[0],time,row[21],row[19][:-4],row[7]]=[row[21],row[19][:-4],row[7],row[2]+" "+row[3],row[9]]
-			#dict[newDays[1],time,row[21],row[19][:-4],row[7]]=[row[21],row[19][:-4],row[7],row[2]+" "+row[3],row[9]]
-		#else:
-			#days=list(row[8])
-			#newDays=convertDay(days)
-			#dict[newDays[0],time,row[21],row[19][:-4],row[7]]=[row[21],row[19][:-4],row[7],row[2]+" "+row[3],row[9]]
-print dict
-print len(dict)
+# {('day(8)','time(9)'): ['course(2,3)','name(7)','prof(19)','room(21)','section(4)']}
+def createDict(PR,queryID):
+	dict={}
+	with open('ecpe.txt','rb') as csv_file:
+		csv_reader=csv.reader(csv_file, delimiter='\t')
+		for row in csv_reader:
+			time=timeConvert(row[9])	
+			if (PR and queryID==row[19]) or (not PR and queryID==row[21]):
+				if row[8]=="MWF":
+					days=list(row[8])
+					newDays=convertDay(days)
+					dict[timeConvert("08:00 am-"+str(row[9][0:8])),newDays[0]]=[time,row[21],row[19][:-4],row[7],row[2]+" "+row[3],row[9],row[4],row[8]]
+					dict[timeConvert("08:00 am-"+str(row[9][0:8])),newDays[1]]=[time,row[21],row[19][:-4],row[7],row[2]+" "+row[3],row[9],row[4],row[8]]
+					dict[timeConvert("08:00 am-"+str(row[9][0:8])),newDays[2]]=[time,row[21],row[19][:-4],row[7],row[2]+" "+row[3],row[9],row[4],row[8]]
+				elif row[8]=="TR":
+					days=list(row[8])
+					newDays=convertDay(days)
+					dict[timeConvert("08:00 am-"+str(row[9][0:8])),newDays[0]]=[time,row[21],row[19][:-4],row[7],row[2]+" "+row[3],row[9],row[4],row[8]]
+					dict[timeConvert("08:00 am-"+str(row[9][0:8])),newDays[1]]=[time,row[21],row[19][:-4],row[7],row[2]+" "+row[3],row[9],row[4],row[8]]
+				elif row[8]=="MW":
+					days=list(row[8])
+					newDays=convertDay(days)
+					dict[timeConvert("08:00 am-"+str(row[9][0:8])),newDays[0]]=[time,row[21],row[19][:-4],row[7],row[2]+" "+row[3],row[9],row[4],row[8]]
+					dict[timeConvert("08:00 am-"+str(row[9][0:8])),newDays[1]]=[time,row[21],row[19][:-4],row[7],row[2]+" "+row[3],row[9],row[4],row[8]]
+				elif row[8]=="WF":
+					days=list(row[8])
+					newDays=convertDay(days)
+					dict[timeConvert("08:00 am-"+str(row[9][0:8])),newDays[0]]=[time,row[21],row[19][:-4],row[7],row[2]+" "+row[3],row[9],row[4],row[8]]
+					dict[timeConvert("08:00 am-"+str(row[9][0:8])),newDays[1]]=[time,row[21],row[19][:-4],row[7],row[2]+" "+row[3],row[9],row[4],row[8]]
+				else:
+					days=list(row[8])
+					newDays=convertDay(days)
+					dict[timeConvert("08:00 am-"+str(row[9][0:8])),newDays[0]]=[time,row[21],row[19][:-4],row[7],row[2]+" "+row[3],row[9],row[8]]
+	print dict
+	print len(dict)
+	print (0,1) in dict
