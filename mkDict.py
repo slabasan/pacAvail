3b32afbf5abaa8d72372c48c1e9e2b60fa279253
Stephanie <s_labasan@u.pacific.edu>
2013-08-25 14:00:00 -0700

    Working mkDict.py

--- mkDict.py
+++ mkDict.py
@@ -35,7 +35,7 @@ def convertDay(x):
   for i in range(len(x)):
     if x[i] == "M":
       x[i] = 1
-        elif x[i] == "T":
+    elif x[i] == "T":
       x[i] = 2
     elif x[i] == "W":
       x[i] = 3
@@ -46,13 +46,13 @@ def convertDay(x):
     return x
   if "TBA" in s:
     return -1
-    else:
-        startH=s[0:2]
-        startM=s[3:6]
-        endH=s[9:11]
-        endM=s[12:15]
-        x=s[6:8]
-        y=s[15:17]
+  else:
+    startH=s[0:2]
+    startM=s[3:6]
+    endH=s[9:11]
+    endM=s[12:15]
+    x=s[6:8]
+    y=s[15:17]
   if x == "am" and y == "pm" and endH != "12":
     newEndH=int(float(endH))+12
     resultH=int(float(newEndH))-int(float(startH))
@@ -91,7 +91,7 @@ def convertDay(x):
 # {('day(8)','time(9)'): ['course(2,3)','name(7)','prof(19)','room(21)','section(4)']}
 def createDict(PR,queryID):
   dict={}
-  with open('ecpe.txt','rb') as csv_file:
+  with open('cleanedClasses.txt','rb') as csv_file:
     csv_reader=csv.reader(csv_file, delimiter='\t')
     for row in csv_reader:
       time=timeConvert(row[9])	
@@ -120,5 +120,5 @@ def createDict(PR,queryID):
         else:
           days=list(row[8])
           newDays=convertDay(days)
-				  dict[timeConvert("08:00 am-"+str(row[9][0:8])),newDays[0]]=[time,row[21],row[19][:-4],row[7],row[2]+" "+row[3],row[9],row[4],row[8]]
+          dict[timeConvert("08:00 am-"+str(row[9][0:8])),newDays[0]]=[time,row[21],row[19][:-4],row[7],row[2]+" "+row[3],row[9],row[4],row[8]]
   return (dict,PR,queryID)
