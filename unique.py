00c6d34ee22fd1a97745175da46f5d82b980b7dd
Stephanie <s_labasan@u.pacific.edu>
2013-08-25 13:25:41 -0700

    Merged stephanie and jay branches to master for deployment. Cleaned up repo.

---
+++ unique.py
@@ -0,0 +1,21 @@
+import csv
+#import creation
+import mkDict
+
+#--- make list of unique professors & rooms
+my_profs=[]
+my_rooms=[]
+with open('cleanedClasses.txt','rb') as csv_file:
+	csv_reader=csv.reader(csv_file, delimiter='\t')
+	my_profs=[row[19][:-4] for row in csv_reader]
+with open('cleanedClasses.txt','rb') as csv_file:
+	csv_reader=csv.reader(csv_file, delimiter='\t')
+	my_rooms=[row[21] for row in csv_reader]
+csv_file.close()
+newProfs=list(set(my_profs))
+newRooms=list(set(my_rooms))
+for prof in newProfs:
+	mkDict.createDict(1,"prof")
+#print newRooms
+#print len(newProfs)
+#print len(newRooms)
